51ee762d45a0397a5cd04f8405bd8f589125bb75
termuxuser01 <53441033+termuxuser01@users.noreply.github.com>
2020-05-22 11:40:00 -0400

    module shanigans

---
+++ mastering_javascript/chapter_1/part_6.js
@@ -0,0 +1,36 @@
+// modules are used to mimic clases and focus on public and private access
+// to variables
+
+// the general structure is 
+// Var moduleName=function() {
+//   //private state
+//   //private functions
+//   return {
+//      //public state
+//      //public variables
+//   }
+// }
+
+// the defined variables will remain private because they are in scope of the function
+
+let superModule = (function (){
+  let secret = "topsecret";
+  let pass = 'nuke123';
+
+  function getSecret(){
+    console.log(secret);
+  }
+
+  function getPass(){
+    console.log(pass)
+  }
+
+  return{
+    getSecret: getSecret,
+    getPass: getPass,
+  }
+
+})();
+
+superModule.getSecret();
+superModule.getPass();
